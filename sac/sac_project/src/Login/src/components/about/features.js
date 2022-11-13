dc60203b4a0dc0d7c5ecb3d9b6a7a1532d37e029
Lucian Popescu <lucian.popescu187@gmail.com>
2022-11-13 15:38:03 +0000

    Delete footer and center features

--- sac/sac_project/src/Login/src/components/about/features.js
+++ sac/sac_project/src/Login/src/components/about/features.js
@@ -11,7 +11,7 @@ function features(){
         <h1 className="feature-head">Features</h1>
         <hr className="fancy-line"/>
       </div>
-      <div className="row center col-xs-12 col-sm-6 col-md-4 col-lg-4">
+      <div className="row center" >
           <img className="img" src={img1} alt="img"/>
           <h3>Ingredients to Recipe</h3>
           <p>Do you have limited ingredients, and don't know what to cook?</p>
